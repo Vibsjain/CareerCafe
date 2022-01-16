7252a3feaf19da0b723c678e38a2250a9ca61938
Vaibhav Gupta <guptavaibhav432@gmail.com>
2022-01-16 15:56:58 +0530

    Fix Errors

--- frontend/src/Components/OurMentors.js
+++ frontend/src/Components/OurMentors.js
@@ -9,84 +9,78 @@ import "../Assets/CSS/Common.css";
 import api from "../api/index";
 
 const MentorCard = (props) => {
-    return (
-        <div className="mentor-main-card mb-12">
-            <div className="card-image">
-                <img
-                    src={props.image}
-                    className="card-image images"
-                    alt="user"
-                />
-            </div>
-            <div className="companyLogo">
-                <img src={props.cImage} className="h-full" />
-            </div>
-            <div className="mentor-info flex flex-col w-full justify-center items-center gap-y-10">
-                <h1 className="mentor-name text-2xl font-semibold">
-                    {props.mentorName}
-                </h1>
-                <h1 className="company-name text-2xl font-semibold">
-                    {props.mentorCompany}
-                </h1>
-            </div>
-        </div>
-    );
+  return (
+    <div className="mentor-main-card mb-12">
+      <div className="card-image">
+        <img src={props.image} className="card-image images" alt="user" />
+      </div>
+      <div className="companyLogo">
+        <img src={props.cImage} className="h-full" />
+      </div>
+      <div className="mentor-info flex flex-col w-full justify-center items-center gap-y-10">
+        <h1 className="mentor-name text-2xl font-semibold">
+          {props.mentorName}
+        </h1>
+        <h1 className="company-name text-2xl font-semibold">
+          {props.mentorCompany}
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default function Mentors() {
-    const [mentors, setMentors] = useState([]);
-    const getMentors = async () => {
-        const response = await api.get("/mentors");
-        setMentors(response.data);
-        console.log(mentors);
-    };
-    useEffect(() => {
-        getMentors();
-    }, []);
+  const [mentors, setMentors] = useState([]);
+  const getMentors = async () => {
+    const response = await api.get("/mentors");
+    setMentors(response.data);
+    console.log(mentors);
+  };
+  useEffect(() => {
+    getMentors();
+  }, []);
 
-    const responsive = {
-        superLargeDesktop: {
-            // the naming can be any, depends on you.
-            breakpoint: { max: 4000, min: 3000 },
-            items: 3,
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3,
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2,
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1,
-        },
-    };
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 3,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
 
-    return (
-        <div className="w-full flex flex-col gap-y-10 justify-center font-bold">
-            <p className="text-5xl text-center text-[#07075F]">Our Mentors</p>
-            <div className="flex w-full md:px-0 sm:px-24">
-                <Carousel
-                    responsive={responsive}
-                    infinite={true}
-                    autoPlay={true}
-                    autoPlaySpeed={3000}
-                    keyBoardControl={true}
-                    removeArrowOnDeviceType={["mobile"]}
-                    containerClass="carousel-container"
-                >
-                    {mentors && mentors.length && mentors.map((card) => {
-                        <MentorCard
-                            image={User}
-                            mentorName={card.name}
-                            mentorCompany={card.company}
-                            cImage={Google}
-                        />;
-                    })}
-                </Carousel>
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full flex flex-col gap-y-10 justify-center font-bold">
+      <p className="text-5xl text-center text-[#07075F]">Our Mentors</p>
+      <div className="flex w-full md:px-0 sm:px-24">
+        <Carousel
+          responsive={responsive}
+          infinite={true}
+          autoPlay={true}
+          autoPlaySpeed={3000}
+          keyBoardControl={true}
+          removeArrowOnDeviceType={["mobile"]}
+          containerClass="carousel-container"
+        >
+          <MentorCard
+            image={User}
+            mentorName="Rishabh"
+            mentorCompany="google"
+            cImage={Google}
+          />
+        </Carousel>
+      </div>
+    </div>
+  );
 }
